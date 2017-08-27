efd2edfb79c420d82ef07ecd19e9aa8c4d9a23fa
pincoin <pincoins@gmail.com>
2017-08-28 02:24:20 +0900

    Functional test now checks we can input a to-do item

--- function_tests.py
+++ function_tests.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +18,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # Edith씨는 페이제 제목과 헤더가 to-do list인 것을 보고 올바르게 방문했음을 안다.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 일정 입력 페이지로 바로 이동한다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # Edith씨 취미는 플라이 낚시로 텍스트박스에 "공작새 깃털 구매"를 입력한다.
+        inputbox.send_keys('공작새 깃털 구매')
 
         # Edith씨가 엔터를 입력하면 페이지를 업데이트하고 목록 페이지를 보여준다.
         # "1: 공작새 깃털 구매"가 일정 목록의 첫 번째 항목이다.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: 공작새 깃털 구매' for row in rows))
 
         # Edith씨는 추가로 텍스트 박스에 입력을 할 수 있고
         # "플라이 만드는데 공작새 깃털 사용"이라고 입력한다.
+        self.fail('Finish the test!')
 
         # 다시 페이지를 업데이트하고 일정 두 가지 모두 목록에 표시한다.
 
